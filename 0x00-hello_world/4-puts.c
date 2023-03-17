9c786658643ebbde48adabadc707e1c6ec62c7d4
LesMuguro <les.vayacom@gmail.com>
2023-03-17 06:10:56 +0000

    Task 4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ * main - this function is meant to display text using the puts function
+ *
+ * Return - a function must always return something
+ */
 
 int main(void) /* Display text using the puts function */
 {
