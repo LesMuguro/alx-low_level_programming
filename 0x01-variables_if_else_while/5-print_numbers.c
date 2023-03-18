a693d98fd09d2adf97be7281fb2c6bea2cea5857
LesMuguro <les.vayacom@gmail.com>
2023-03-18 19:35:49 +0000

    Task 5 Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 10; n++)
 	{
-		putchar(n);
+		printf("%d", n);
 	}
 
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
