1599ddd2899768bca3f0364aaaf7a1cacf232db2
LesMuguro <les.vayacom@gmail.com>
2023-03-19 12:30:08 +0000

    Task 7 Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point and function to print alphabet in reverse
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 97; a--)
+	{
+		putchar(n - 122);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
