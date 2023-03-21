294fc9ccb082b988f6ee5a37ee4ee879c66a84c0
LesMuguro <les.vayacom@gmail.com>
2023-03-21 14:12:38 +0000

    Task 0 _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
-
+/**
+ * main - entry point and function to print "_putchar"
+ * using _putchar function
+ *
+ *Return: always 0
+ */
 int main(void)
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
