98b04ad1dbf2dae7afa09c204e25026decf8d4ee
LesMuguro <les.vayacom@gmail.com>
2023-03-21 15:43:08 +0000

    Task 1 Insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,17 +10,19 @@
 void print_alphabet(void)
 {
 	int a;
+	int print_alphabet = a;
 
 	for (a = 97; a <= 122; a++)
 	{
 		_putchar(a);
 		_putchar('\n');
 	}
+
 }
 
 int main(void)
 {
-	print_alphabet();
+	_putchar(print_alphabet());
 
 	return (0);
 }
