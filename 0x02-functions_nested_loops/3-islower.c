88011a19398f9a6f40796df1c0ba7f061f03c8e9
LesMuguro <les.vayacom@gmail.com>
2023-03-21 19:11:55 +0000

    Task 3 islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,21 +4,18 @@
  * _islower - displays 1 if the input is a lowercase
  * character. Other cases show 0
  *
- * c: The character in ASCII code
+ * @c: The character in ASCII code
  *
  * Return: always 1 for lowercase and 0 for the rest
  */
 int _islower(int c)
 {
-	for (c = 97; c <= 122; c++)
-	{
-		_putchar(48);
-	}
-
-	for (c = 65; c <= 90; c++)
+	if (c <= 122 && c >= 97)
 	{
 		_putchar(49);
 	}
-
-	return (0);
+	else
+	{
+		_putchar(48);
+	}
 }
