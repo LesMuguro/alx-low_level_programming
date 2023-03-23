7dd72684933ee5c6df9ae4ce58a5e7877cbb6795
LesMuguro <les.vayacom@gmail.com>
2023-03-23 11:41:58 +0000

    Task 2 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,11 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = 0;
-
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
