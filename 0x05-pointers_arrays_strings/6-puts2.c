04a74d11db1560a4999c96e905dab2c5e48a0ca7
LesMuguro <les.vayacom@gmail.com>
2023-03-28 18:01:17 +0000

    Task 6 half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,16 @@
  */
 void puts2(char *str)
 {
-	long int i = 0;
+	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
 	}
 
 	_putchar('\n');
