ae73d23f0e07ff925b74972a6c90b6a98b12efb1
LesMuguro <les.vayacom@gmail.com>
2023-03-28 06:07:06 +0000

    Task 0 98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to turn a number into 98
+ *
+ * @n: pointer
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+
+	_putchar(98);
+}
