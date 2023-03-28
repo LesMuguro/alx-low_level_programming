8f8e14d4df51974fadf94e5c2ab876c92e1d1a1f
LesMuguro <les.vayacom@gmail.com>
2023-03-28 17:45:47 +0000

    Task 6 half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character in a
+ * string starting with the first character
+ *
+ * @str: pointer to take the string
+ *
+ * Return: no return value
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
