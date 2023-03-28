a2a2263f10fe40ca8f7c4f058b6771677bb6c9f3
LesMuguro <les.vayacom@gmail.com>
2023-03-28 08:16:37 +0000

    Task 2 this report ,by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to print the length of a string
+ *
+ * @s: pointer
+ *
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	while (*s != 0)
+	{
+		*s++;
+	}
+
+	return (0);
+}
