2b4600ab5faf21b5daf2e79b0e45bcbc5a24906a
LesMuguro <les.vayacom@gmail.com>
2023-03-30 07:21:21 +0000

    Task 2 strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy a string
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: number of bytes
+ *
+ * Return: destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
+	}
+
+	while (b < n)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,4 @@
 Task 0: A function that concatenates 2 strings.
 Task 1: A frunction that concatenates 2 strings.
+Task 2: A function that copies a string.
+
