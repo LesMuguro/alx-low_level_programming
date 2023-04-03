459ab2b9a3ebf4291d03c36ffdc388bf85331b30
LesMuguro <les.vayacom@gmail.com>
2023-04-03 17:20:46 +0000

    Task 0 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - function to fill memory with a constant byte
+ * *_memset - function to fill memory with a constant byte
  *
  * @s: pointer to memory area
  * @b: constant byte
