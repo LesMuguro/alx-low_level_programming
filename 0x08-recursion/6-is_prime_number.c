cbb1b951b07a93bd915c869b2e9c2190d053f6c0
LesMuguro <les.vayacom@gmail.com>
2023-04-07 03:02:57 +0000

    Task 6 inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,24 +3,42 @@
 int is_divisible(int num, int div);
 
 /**
- * is_prime_number - function to print the prime number
+ * is_prime_number - function to print if a number is a prime number or not
  *
- * @n: the input
+ * @n: the number to evaluate
  *
- * Return: return 0 is the number is divisible or -1 if the
- * number is not divisible
+ * Return: return 1 if n is a prime number, and 0 if not prime
  */
 int is_prime_number(int n)
 {
-	if (num % div == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
 
-	if (div == num / 2)
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - function that calculates if a number is prime
+ * recursively
+ *
+ * @n: number to evaluate
+ * @i: iteration
+ *
+ * Return: returns 1 if n is prime, and 0 if not prime
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
 	{
 		return (1);
 	}
 
-	return (is_divisible(num, div + 1));
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, i - 1));
 }
