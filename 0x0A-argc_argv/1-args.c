ca305c411e59b7fa3c3d6c807378abd1071f4f53
LesMuguro <les.vayacom@gmail.com>
2023-04-10 17:41:47 +0000

    Task 1 silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of arguments
+ * passed into it
+ *
+ * @argc: number of arguments
+ * @argv: unused
+ *
+ * Return: always 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 C - argc, argv
 
 Task 0: A program that prints its name, followed by a new line.
+Task 1: A program that prints the number of arguments passed into it.
