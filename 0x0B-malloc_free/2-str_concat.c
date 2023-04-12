af0d1aeb3677ebe3e2fc9fc52da572a73895ba53
LesMuguro <les.vayacom@gmail.com>
2023-04-12 05:58:39 +0000

    Task 2: he who is not courageious enough

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,32 +14,22 @@ char *str_concat(char *s1, char *s2)
 	int i, orig;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	i = orig = 0;
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 
 	while (s2[orig] != '\0')
-	{
 		orig++;
-	}
 
 	concat = malloc(sizeof(char) * (i + orig + 1));
 
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
 
 	i = orig = 0;
 	while (s1[i] != '\0')
