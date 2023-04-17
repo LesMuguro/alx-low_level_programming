a9fed6cb8d7abacecb388092db5e5e2763fd29cb
LesMuguro <les.vayacom@gmail.com>
2023-04-17 14:49:44 +0000

    Task 0: Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+0x0E. C- Structures, typedef
+
+Task 0: a new type of struct dog with some elements: name, age and owner.
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+
+#ifndef STDIO_H
+#define STDIO_H
+/**
+ * struct dog - struct for name, age and owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: this is a struct for a dog's name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
