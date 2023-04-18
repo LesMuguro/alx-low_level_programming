c321fb28ad53120c2c513f8d00078ee5953744d2
LesMuguro <les.vayacom@gmail.com>
2023-04-18 04:54:35 +0000

    Task 3: Outside of a dog, a book is a man's best friend.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,22 @@ struct dog
 	char *owner;
 };
 
+
+/**
+ * struct dog_t - struct for name, age and owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: this is a new name for the type struct dog
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
