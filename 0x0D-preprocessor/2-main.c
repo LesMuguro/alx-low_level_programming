4d5ebf020be4deb1083e931a3e77c7ad7162e11d
LesMuguro <les.vayacom@gmail.com>
2023-04-19 07:51:16 +0000

    Task 2 File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print the name of the name of the file
+ * it was compiled from, followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
